46bc711013807cc106f3b101cc8bedfaf86d7d5d
FilipeAbdo <filipeibraim@gmail.com>
2024-04-15 20:07:12 -0300

    updated so the port is no more needed

--- HTTPHelloWorld/app.js
+++ HTTPHelloWorld/app.js
@@ -2,6 +2,6 @@ var http = require("http");
 
 http.createServer(function(req, resp){
     resp.end("<h1>Bem vindo ao meu site!</h1><h4>www.guiadoprogramador.com</h4>");
-}).listen(8181);
+}).listen(80);
 
 console.log("Meu servdor está rodando!")

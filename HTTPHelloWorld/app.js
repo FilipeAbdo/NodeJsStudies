09cf16484fab932138628e4953531898479aecd4
FilipeAbdo <filipeibraim@gmail.com>
2024-04-15 19:59:38 -0300

    Adding HTTHelloWorld project

---
+++ HTTPHelloWorld/app.js
@@ -0,0 +1,7 @@
+var http = require("http");
+
+http.createServer(function(req, resp){
+    resp.end("<h1>Bem vindo ao meu site!</h1><h4>www.guiadoprogramador.com</h4>");
+}).listen(8181);
+
+console.log("Meu servdor está rodando!")
